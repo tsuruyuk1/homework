7eed5c3ede2b18f8dc4dcab4485ec86c0dafa9e0
tsuruyuk1 <48685606+tsuruyuk1@users.noreply.github.com>
2025-09-18 17:37:00 +0300

    Add files via upload

--- layout3/js/main.js
+++ layout3/js/main.js
@@ -174,3 +174,13 @@ const reviewsSwiper = new Swiper('.reviews__slider', {
 });
 
 // SLIDER-REWIEWS
+
+// TEL-MASK
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+
+    im.mask(telInputs)
+
+// TEL-MASK
+
